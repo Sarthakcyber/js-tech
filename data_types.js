9b07974850cbb5c787107c8822074a5825b9b8cb
Sarthak Agrawal <sarthakagrawal282@gmail.com>
2025-10-16 14:22:33 +0000

    discuss

---
+++ data_types.js
@@ -0,0 +1,19 @@
+let name = "Sarthak"
+let age = 18
+let is_logged = false
+let state = null
+
+console.table([ name, age , is_logged])
+
+/*
+PRIMITIVE DATA TYPES ->
+number -> 2 to power 53
+bigint 
+string -> ""
+boolean -> True/False
+null -> standalone value // if no value recived then null // null is an object 
+undefined -> no value assigned // is a data type
+symbol ->  unique 
+*/
+
+console.log(typeof is_logged)
