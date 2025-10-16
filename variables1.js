37d913ec495a7b43e6b6aa7098ee10c4f015c837
Sarthak Agrawal <sarthakagrawal282@gmail.com>
2025-10-16 14:01:29 +0000

    change2

---
+++ variables1.js
@@ -0,0 +1,22 @@
+const accountid = 14453
+let accountemail = "sarthak@gmail.com"
+var accountpass = "12345"
+accountcity = "Jaipur"
+
+// accountid = "String"
+accountemail = "hello@gmail.com"
+accountpass = "214532"
+accountpass = "Noida"
+
+/*
+inside everthing is comment 
+we will not be using the var for variable because of block scope and functional scope {
+
+}
+as it earlier version it does not support js
+
+*/
+
+console.log(accountid)
+console.table([accountid , accountemail , accountpass , accountcity])
+
