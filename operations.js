fc4441462e65c9d5d935b3296a07fee51e0dbacc
Sarthak Agrawal <sarthakagrawal282@gmail.com>
2025-10-17 00:16:20 +0000

    operations done

---
+++ operations.js
@@ -0,0 +1,35 @@
+let num = 33 
+let stringnum = String(num)
+console.log(stringnum)
+console.log(typeof stringnum)
+
+// operations \
+
+let val = 3
+let negval = -val
+console.log(negval)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log("power: " , 2**3)
+console.log(2/2)
+console.log(2%2)
+
+
+let str1 = "chai "
+let str2 = "code"
+let str3 = str1 + "aur " + str2
+console.log(str3)
+
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 +2)
+console.log(1 + 2 + "2")
+
+console.log(+true)
+console.log(true)
+//console.log(true +) // error because joins are not happening fully operationable
+
+
