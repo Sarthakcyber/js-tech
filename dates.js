e254487f19eba5b2806b24b6243efb2194c405bf
Sarthak Agrawal <sarthakagrawal282@gmail.com>
2025-10-31 21:40:55 +0000

    dates

---
+++ dates.js
@@ -0,0 +1,11 @@
+console.log(3+5);
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+
+console.log(typeof myDate);
+
+let myCretedate = new Date (2023,0,1) //month satrt with 0 in js 
+console.log(myCretedate.toDateString());
