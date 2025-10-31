91546d5ac7ad15eac7dc9c852dbfe697ebaa2e35
Sarthak Agrawal <sarthakagrawal282@gmail.com>
2025-10-31 20:07:49 +0000

    numbers done

--- number.js
+++ number.js
@@ -1,1 +1,41 @@
 console.log("Sarthak");
+
+const score = 100
+
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toString());
+
+// console.log(typeof(balance));
+
+const otherNumber = 23.558
+const Pno = 1123.6523
+const hundred = 10000000 
+
+// console.log(otherNumber.toPrecision(3)); // gives the precise value upto given no.
+// console.log(Pno.toPrecision(3));
+// console.log(hundred.toLocaleString(3)); // seperate the number based on given US number system
+// console.log(hundred.toLocaleString('en-IN')); //changes the region from US number system to INDIAN number system
+
+////////+++++++++++++++ MATHS +++++++++++ //////////////
+
+// console.log(Math);
+// console.log(Math.abs(-4)); // changes the negative to positive
+// console.log(Math.round(4.5)); //round of
+// console.log(Math.ceil(4.2)); // gives the top val
+// console.log(Math.floor(4.2)); // gives the lowest posssible val
+// console.log(Math.sqrt(25)); 
+// console.log(Math.min(4,55,5334,234,6));
+// console.log(Math.max(4,55,5334,234,6));
+
+console.log(Math.random(1)*6); //always comes between 0-1
+console.log((Math.random()*10) +1); // shifted to left added 1 to avoid 0
+console.log(Math.floor(Math.random()*10) +1); // 
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min+1) + min)) // used for given max and min val used 1st to shift left then added 1 to avoid 0 and then added with min val to achieve a val
+
